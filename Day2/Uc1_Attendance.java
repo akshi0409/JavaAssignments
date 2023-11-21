75359298d8632a7357f533a53a3b9edef6fe7288
akshi0409 <akshita.asah@gmail.com>
2023-11-21 20:06:39 +0530

    Added Day2 Practice Problem UC1 - Attendance

---
+++ Day2/Uc1_Attendance.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Uc1_Attendance
+{
+	public static void main(String[] args)
+	{
+		int employeeid;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Employee ID");
+		employeeid = sc.nextInt();
+		Random random = new Random();
+		boolean attd = random.nextBoolean();
+		if(attd)
+		{
+			System.out.println("The employee is present");
+		}
+		else
+		{
+			System.out.println("The employee is absent");
+		}
+	}
+}

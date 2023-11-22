66f1222bf53cb7bdcffa0b73b1e75ef6665c9381
akshi0409 <akshita.asah@gmail.com>
2023-11-22 14:58:40 +0530

    Refactored : Added the UC4 - Switch Case Statements

--- Day2/EmployeeWage.java
+++ Day2/EmployeeWage.java
@@ -3,25 +3,22 @@ class EmployeeWage
 {
 	public static void main(String args[])
 	{
-		int IS_FULL_TIME = 1;
-	        int IS_PART_TIME = 2;
 		int WAGE_PER_HOUR = 20;
 		System.out.println("Welcome to Employee Wage Computation Problem");
-		double empAttendance = Math.floor(Math.random() * 10) % 3;
+		int empAttendance = (int) Math.floor(Math.random() * 10) % 3;
 		int empHours = 0;
-		if(empAttendance == IS_FULL_TIME)
+		switch(empAttendance)
 		{
-			System.out.println("Employee is present and is full time");
-			empHours = 8;
-		}
-		else if(empAttendance == IS_PART_TIME)
-		{
-			System.out.println("Employee is present and is part time.");
-			empHours = 4;
-		}
-		else
-		{
-			System.out.println("Employee is absent");
+			case 1:
+				System.out.println("Employee is present and is full time");
+				empHours = 8;
+				break;
+			case 2:
+				System.out.println("Employee is present and is part time.");
+				empHours = 4;	
+				break;
+			default:
+				System.out.println("Employee is absent");
 		}
 		System.out.println("The employee wage is: "+ empHours*WAGE_PER_HOUR );
 	}

b98fdba3a3746714ebdfa4dbb468ea570f7e440c
akshi0409 <akshita.asah@gmail.com>
2023-11-22 15:03:36 +0530

    Refactored : Added the UC5 - Monthly Wage Computation

--- Day2/EmployeeWage.java
+++ Day2/EmployeeWage.java
@@ -4,22 +4,26 @@ class EmployeeWage
 	public static void main(String args[])
 	{
 		int WAGE_PER_HOUR = 20;
+		int WORKING_DAYS_COUNT = 30;
 		System.out.println("Welcome to Employee Wage Computation Problem");
-		int empAttendance = (int) Math.floor(Math.random() * 10) % 3;
 		int empHours = 0;
-		switch(empAttendance)
+		for(int i=0;i<WORKING_DAYS_COUNT;i++)
 		{
-			case 1:
-				System.out.println("Employee is present and is full time");
-				empHours = 8;
-				break;
-			case 2:
-				System.out.println("Employee is present and is part time.");
-				empHours = 4;	
-				break;
-			default:
-				System.out.println("Employee is absent");
+			int empAttendance = (int) Math.floor(Math.random() * 10) % 3;
+			switch(empAttendance)
+			{
+				case 1:
+					System.out.println("Employee is present and is full time");
+					empHours+=8;
+					break;
+				case 2:
+					System.out.println("Employee is present and is part time.");
+					empHours+=4;	
+					break;
+				default:
+					System.out.println("Employee is absent");
+			}
 		}
-		System.out.println("The employee wage is: "+ empHours*WAGE_PER_HOUR );
+		System.out.println("The monthly employee wage is: "+ empHours*WAGE_PER_HOUR );
 	}
 }

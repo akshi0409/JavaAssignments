bf81bdbd7e171a37fb9be630fe79644132323ad0
akshi0409 <akshita.asah@gmail.com>
2023-11-22 14:38:33 +0530

    Refactored : Added the UC3 - Part Time Wage Computation

--- Day2/EmployeeWage.java
+++ Day2/EmployeeWage.java
@@ -3,15 +3,21 @@ class EmployeeWage
 {
 	public static void main(String args[])
 	{
-		int FULL_TIME = 1;
+		int IS_FULL_TIME = 1;
+	        int IS_PART_TIME = 2;
 		int WAGE_PER_HOUR = 20;
 		System.out.println("Welcome to Employee Wage Computation Problem");
-		double empAttendance = Math.floor(Math.random() * 10) % 2;
+		double empAttendance = Math.floor(Math.random() * 10) % 3;
 		int empHours = 0;
-		if(empAttendance == FULL_TIME)
+		if(empAttendance == IS_FULL_TIME)
 		{
-			System.out.println("Employee is present");
+			System.out.println("Employee is present and is full time");
 			empHours = 8;
+		}
+		else if(empAttendance == IS_PART_TIME)
+		{
+			System.out.println("Employee is present and is part time.");
+			empHours = 4;
 		}
 		else
 		{

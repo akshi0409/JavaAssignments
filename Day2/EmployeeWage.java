4ffeb0d74606ec7c03975b948fbc7d2d34964353
akshi0409 <akshita.asah@gmail.com>
2023-11-22 15:08:32 +0530

    Refactored : Added the UC6 - Conditional Wage Computation

--- Day2/EmployeeWage.java
+++ Day2/EmployeeWage.java
@@ -6,9 +6,10 @@ class EmployeeWage
 		int WAGE_PER_HOUR = 20;
 		int WORKING_DAYS_COUNT = 30;
 		System.out.println("Welcome to Employee Wage Computation Problem");
-		int empHours = 0;
-		for(int i=0;i<WORKING_DAYS_COUNT;i++)
+		int empHours = 0, workingDays=0;
+		while(empHours<=100 && workingDays<=WORKING_DAYS_COUNT)
 		{
+			workingDays++;
 			int empAttendance = (int) Math.floor(Math.random() * 10) % 3;
 			switch(empAttendance)
 			{

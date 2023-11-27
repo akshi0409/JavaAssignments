9aa4e4d551a032601366de60ff8a215ca30e57ce
akshi0409 <akshita.asah@gmail.com>
2023-11-27 15:59:00 +0530

    Added theory questions

--- Review_Week1/ReverseNumber.java
+++ Review_Week1/ReverseNumber.java
@@ -17,3 +17,16 @@ public class ReverseNumber
 		System.out.println("The reversed number is: "+reversedNumber);
 	}
 }
+
+/*Theory questions:
+Uses of static keyword : 
+1) Use to refer to common functions without creating objects.
+2) For memory management, static variable gets memory only once during loading.
+Constructors and its types:
+It is a special function used for initializing the class variables. It is called directly when an object of the class is created. IT has same name as the class name.
+Types:
+Default Constructor : When no constructor is defined, the compiler inserts a constructor on its own.
+Unparameterized Constructor : Which does not receive any values as parameters for initialization.
+Parameterized Constructor : Which receives values as parameters.
+Copy Constructor : Which intializes one object with another object.
+*/

5d20ae1a642464982c2428ecacf0c10da4b788a4
akshi0409 <akshita.asah@gmail.com>
2023-11-29 21:18:19 +0530

    Refactored : Added the Test Case for multiple paramters

--- Day8/MaxValue.java
+++ Day8/MaxValue.java
@@ -26,32 +26,51 @@ public class MaxValue <E extends Comparable<E>>{
     }
     public static void main(String[] args)
     {
-        Scanner sc= new Scanner(System.in);
-        int n;
-        System.out.println("Enter the number of values to compare for integer: ");
-        n = sc.nextInt();
-        System.out.println("Enter the strings: ");
-        Integer[] intArray = new Integer[n];
-        for(int i=0;i<n;i++) {
-            intArray[i] = sc.nextInt();
-        }
-        System.out.println("Enter the number of values to compare for float: ");
-        n = sc.nextInt();
-        System.out.println("Enter the values: ");
-        Float[] floatArray = new Float[n];
-        for(int i=0;i<n;i++) {
-            floatArray[i] = sc.nextFloat();
-        }
-        System.out.println("Enter the number of values to compare for string: ");
-        n = sc.nextInt();
-        System.out.println("Enter the strings: ");
-        String[] stringArray = new String[n];
-        for(int i=0;i<n;i++) {
-            stringArray[i] = sc.next();
-        }
-        new MaxValue<Integer>(intArray).findMax();
-        new MaxValue<Float>(floatArray).findMax();
-        new MaxValue<String>(stringArray).findMax();
+          //TC1
+          Integer[] intArray1 = {4,2,3,2};
+          Float[] floatArray1 = {4.5f,1.2f,3.4f};
+          String[] stringArray1 = {"ef","cd","ab"};
+          new MaxValue<Integer>(intArray1).findMax();
+          new MaxValue<Float>(floatArray1).findMax();
+          new MaxValue<String>(stringArray1).findMax();
+          //TC2
+          Integer[] intArray2 = {4,6,3,2};
+          Float[] floatArray2 = {4.5f,7.2f,3.4f};
+          String[] stringArray2 = {"ab","yz","cd"};
+          new MaxValue<Integer>(intArray2).findMax();
+          new MaxValue<Float>(floatArray2).findMax();
+          new MaxValue<String>(stringArray2).findMax();
+          //TC3
+          Integer[] intArray3 = {2,1,3,2};
+          Float[] floatArray3 = {4.5f,1.2f,9.4f};
+          String[] stringArray3 = {"ab","cd","ef"};
+          new MaxValue<Integer>(intArray3).findMax();
+          new MaxValue<Float>(floatArray3).findMax();
+          new MaxValue<String>(stringArray3).findMax();
+//        Code for program using user input:
+ //        Scanner sc= new Scanner(System.in);
+//        int n;
+//        System.out.println("Enter the number of values to compare for integer: ");
+//        n = sc.nextInt();
+//        System.out.println("Enter the strings: ");
+//        Integer[] intArray = new Integer[n];
+//        for(int i=0;i<n;i++) {
+//            intArray[i] = sc.nextInt();
+//        }
+//        System.out.println("Enter the number of values to compare for float: ");
+//        n = sc.nextInt();
+//        System.out.println("Enter the values: ");
+//        Float[] floatArray = new Float[n];
+//        for(int i=0;i<n;i++) {
+//            floatArray[i] = sc.nextFloat();
+//        }
+//        System.out.println("Enter the number of values to compare for string: ");
+//        n = sc.nextInt();
+//        System.out.println("Enter the strings: ");
+//        String[] stringArray = new String[n];
+//        for(int i=0;i<n;i++) {
+//            stringArray[i] = sc.next();
+//        }
     }
 }
 /* Test cases:

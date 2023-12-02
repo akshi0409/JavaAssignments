90b06adbb2c182a0459e6077ef550c5ae336fed6
akshi0409 <akshita.asah@gmail.com>
2023-12-02 15:44:18 +0530

    Added UC3 Enqueue Operation

---
+++ Day9/queue.java
@@ -0,0 +1,48 @@
+class QueueNode
+{
+    int value;
+    QueueNode link;
+    QueueNode(int val)
+    {
+        this.value = val;
+        link = null;
+    }
+}
+public class queue {
+    QueueNode head;
+    void enqueue(int value)
+    {
+        QueueNode n = new QueueNode(value);
+        if(head == null)
+            head = n;
+        else
+        {
+            QueueNode temp = head;
+            while(temp.link!=null)
+            {
+                temp=temp.link;
+            }
+            temp.link = n;
+        }
+    }
+    void printList()
+    {
+        QueueNode temp = head;
+        while(temp.link!=null) {
+            System.out.print(temp.value+"->");
+            temp = temp.link;
+        }
+        System.out.println(temp.value);
+    }
+    public static void main(String[] args)
+    {
+        queue q = new queue();
+        q.enqueue(56);
+        q.enqueue(30);
+        q.enqueue(70);
+        System.out.println("The list after enqueue operation");
+        q.printList();
+    }
+}
+
+

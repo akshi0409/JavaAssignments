cad57fad6d5a8a28835436dece694b68b0ce4bf1
akshi0409 <akshita.asah@gmail.com>
2023-12-04 13:47:39 +0530

    Added UC1 Adding the key value pairs

---
+++ Day10/LinkedHashMap.java
@@ -0,0 +1,62 @@
+class MyMapNode{
+    String key;
+    int value;
+    MyMapNode next;
+    MyMapNode(String s)
+    {
+        key = s;
+        value = 1;
+        next = null;
+    }
+}
+public class LinkedHashMap {
+    MyMapNode head;
+    void addNode(String word)
+    {
+        if(head == null)
+        {
+            MyMapNode newNode = new MyMapNode(word);
+            head = newNode;
+            return;
+        }
+        MyMapNode temp = head;
+        while(temp.next!=null)
+        {
+            if(temp.key.equals(word))
+            {
+                temp.value++;
+                return;
+            }
+            temp=temp.next;
+        }
+        if(temp.key.equals(word))
+        {
+            temp.value++;
+            return;
+        }
+        MyMapNode newNode = new MyMapNode(word);
+        temp.next = newNode;
+    }
+    void display()
+    {
+        System.out.println("The elements of the hash map are: ");
+        MyMapNode temp = head;
+        while(temp!=null)
+        {
+            System.out.println(temp.key+" "+temp.value);
+            temp = temp.next;
+        }
+    }
+    public static void main(String[] args)
+    {
+        LinkedHashMap wordMap = new LinkedHashMap();
+        String s = "To be or not to be";
+        s = s.toLowerCase();
+        String words[] = s.split(" ");
+        for(String word:words)
+        {
+            wordMap.addNode(word);
+        }
+        wordMap.display();
+    }
+}

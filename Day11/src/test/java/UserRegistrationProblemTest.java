afbcaf348e57c4d756582d4cac8e7feca47b87ad
akshi0409 <akshita.asah@gmail.com>
2023-12-07 00:04:24 +0530

    Refactored : Changed the print statements

--- Day11/src/test/java/UserRegistrationProblemTest.java
+++ Day11/src/test/java/UserRegistrationProblemTest.java
@@ -23,25 +23,25 @@ public class UserRegistrationProblemTest {
     public void MailTest()
     {
         UserRegistrationProblem mail = new UserRegistrationProblem();
-        Assert.assertTrue("The name is valid",mail.matchMail("akshita.asah@gmail.com"));
-        Assert.assertFalse("The name is valid",mail.matchMail("akshita.asah@gmail.com.com"));
-        Assert.assertFalse("The name is valid",mail.matchMail("akshita.asah.asah@gmail.com.in"));
+        Assert.assertTrue("The mail is valid",mail.matchMail("xyz.abc@gmail.com"));
+        Assert.assertFalse("The mail is valid",mail.matchMail("xyz.abc@gmail.com.com"));
+        Assert.assertFalse("The mail is valid",mail.matchMail("xyz.abc.abc@gmail.com.in"));
     }
     @Test
     public void MobileTest()
     {
         UserRegistrationProblem number = new UserRegistrationProblem();
-        Assert.assertTrue("The name is valid",number.matchNumber("91 9876543210"));
-        Assert.assertFalse("The name is valid",number.matchNumber("+91 9876543210"));
-        Assert.assertFalse("The name is valid",number.matchNumber("919876543210"));
+        Assert.assertTrue("The number is valid",number.matchNumber("91 9876543210"));
+        Assert.assertFalse("The number is valid",number.matchNumber("+91 9876543210"));
+        Assert.assertFalse("The number is valid",number.matchNumber("919876543210"));
     }
     @Test
     public void passwordTest()
     {
         UserRegistrationProblem password = new UserRegistrationProblem();
-        Assert.assertTrue("The name is valid",password.matchPassword("w9E3+4q5u"));
-        Assert.assertFalse("The name is valid",password.matchPassword("w9E34q5u"));
-        Assert.assertFalse("The name is valid",password.matchPassword("w9E3+4"));
+        Assert.assertTrue("The password is valid",password.matchPassword("w9E3+4q5u"));
+        Assert.assertFalse("The password is valid",password.matchPassword("w9E34q5u"));
+        Assert.assertFalse("The password is valid",password.matchPassword("w9E3+4"));
     }
 
 }
--- Day_12/UserRegistration/src/test/java/UserRegistrationTest.java
+++ Day_12/UserRegistration/src/test/java/UserRegistrationTest.java
@@ -22,25 +22,25 @@ public class UserRegistrationTest {
     public void MailTest()
     {
         UserRegistration mail = new UserRegistration();
-        Assert.assertTrue("The name is valid",mail.matchMail("xyz.abc@gmail.com"));
-        Assert.assertFalse("The name is valid",mail.matchMail("xyz.abc@gmail.com.com"));
-        Assert.assertFalse("The name is valid",mail.matchMail("xyz.abc.asah@gmail.com.in"));
+        Assert.assertTrue("The mail is valid",mail.matchMail("xyz.abc@gmail.com"));
+        Assert.assertFalse("The mail is valid",mail.matchMail("xyz.abc@gmail.com.com"));
+        Assert.assertFalse("The mail is valid",mail.matchMail("xyz.abc.asah@gmail.com.in"));
     }
     @Test
     public void MobileTest()
     {
         UserRegistration number = new UserRegistration();
-        Assert.assertTrue("The name is valid",number.matchNumber("91 999999999"));
-        Assert.assertFalse("The name is valid",number.matchNumber("+91 9999999999"));
-        Assert.assertFalse("The name is valid",number.matchNumber("919888888888"));
+        Assert.assertTrue("The number is valid",number.matchNumber("91 999999999"));
+        Assert.assertFalse("The number is valid",number.matchNumber("+91 9999999999"));
+        Assert.assertFalse("The number is valid",number.matchNumber("919888888888"));
     }
     @Test
     public void passwordTest()
     {
         UserRegistration password = new UserRegistration();
-        Assert.assertTrue("The name is valid",password.matchPassword("w9E3+4q5u"));
-        Assert.assertFalse("The name is valid",password.matchPassword("w9E34q5u"));
-        Assert.assertFalse("The name is valid",password.matchPassword("w9E3+4"));
+        Assert.assertTrue("The password is valid",password.matchPassword("w9E3+4q5u"));
+        Assert.assertFalse("The password is valid",password.matchPassword("w9E34q5u"));
+        Assert.assertFalse("The password is valid",password.matchPassword("w9E3+4"));
     }
 
 }

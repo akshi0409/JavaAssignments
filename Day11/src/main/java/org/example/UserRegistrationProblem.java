5e01ef2e45787a7dd285c478d9901e0c58488dc4
akshi0409 <akshita.asah@gmail.com>
2023-12-06 15:35:23 +0530

    Refactored : Added Uc6 to check capital letter

--- Day11/src/main/java/org/example/UserRegistrationProblem.java
+++ Day11/src/main/java/org/example/UserRegistrationProblem.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class UserRegistrationProblem {
     public boolean matchName(String firstName) {
-        return (Pattern.matches("^[A-Z][a-z].{3,}$", firstName));
+        return (Pattern.matches("^[A-Z][a-z].{2,}$", firstName));
     }
     public boolean matchMail(String firstName){
         return (Pattern.matches("^[A-Za-z0-9]+"+"([._]+[A-za-z0-9]+)*"+"@"+"[A-Za-z0-9]+"+"."+"([A-za-z0-9]+[.])*"+"[A-Za-z0-9]*",firstName));
@@ -14,12 +14,12 @@ public class UserRegistrationProblem {
         return (Pattern.matches("^[0-9]{1,2}\\s[1-9][0-9]{9}",number));
     }
     public boolean matchPassword(String firstName){
-        return (Pattern.matches("^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&-+=()]).{8,}$",firstName));
+        return (Pattern.matches("(?=.*[A-Z]).{8,}$",firstName));
     }
     public static void main(String[] args) {
         UserRegistrationProblem user = new UserRegistrationProblem();
         System.out.println(user.matchName("Akshita"));
-        System.out.println(user.matchName("Ak"));
+        System.out.println(user.matchName("Aks"));
         System.out.println(user.matchMail("abcxyz@bl.co.co.in"));
         System.out.println(user.matchNumber("09046371890"));
         System.out.println(user.matchPassword("9wUf@fabcdeeee"));

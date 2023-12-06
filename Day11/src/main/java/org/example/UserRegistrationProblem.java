4618cda1e89f698b53a9fa93ebe1de811d03b173
akshi0409 <akshita.asah@gmail.com>
2023-12-06 18:02:32 +0530

    Added UC10 Junit Tests for each function

--- Day11/src/main/java/org/example/UserRegistrationProblem.java
+++ Day11/src/main/java/org/example/UserRegistrationProblem.java
@@ -7,14 +7,14 @@ public class UserRegistrationProblem {
         return (Pattern.matches("^[A-Z][a-z]{2,}$", name));
     }
     public boolean matchMail(String mail){
-        return (Pattern.matches("^[A-Za-z0-9]+"+"([._]+[A-za-z0-9]+)*"+"@"+"[A-Za-z0-9]+"+"."+"([A-za-z0-9]+[.])*"+"[A-Za-z0-9]*",mail));
+        return (Pattern.matches("^[A-Za-z0-9]+"+"([+._-][A-za-z0-9]+)?"+"@[A-Za-z0-9]+"+"[.]([A-za-z0-9])+"+"([.][A-Za-z0-9]{0,2})?",mail));
     }
     public boolean matchNumber(String number)
     {
         return (Pattern.matches("^[0-9]{1,2}\\s[1-9][0-9]{9}",number));
     }
     public boolean matchPassword(String password){
-        return (Pattern.matches("(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&-+=()]).{8,}$",password));
+        return (Pattern.matches("^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&-+=()]).{8,}$",password));
     }
     public static void main(String[] args) {
         UserRegistrationProblem user = new UserRegistrationProblem();
@@ -22,6 +22,6 @@ public class UserRegistrationProblem {
         System.out.println(user.matchName("Aks"));
         System.out.println(user.matchMail("abcxyz@bl.co.co.in"));
         System.out.println(user.matchNumber("09046371890"));
-        System.out.println(user.matchPassword("9wUf@fabcdeeee"));
+        System.out.println(user.matchPassword("wUf@9fabcdeeee"));
     }
 }
